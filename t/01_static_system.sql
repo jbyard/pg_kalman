8a02506e883c2e2678be767e27600593f0567e2a
Josh Byard <josh@winstonthefish.com>
2021-10-12 13:05:33 -0400

    test didnt make much sense

--- t/01_static_system.sql
+++ t/01_static_system.sql
@@ -1,5 +1,5 @@
 BEGIN;
-SELECT plan(3);
+SELECT plan(2);
 
 
 SELECT is((
@@ -9,11 +9,6 @@ SELECT is((
 SELECT is((
 	SELECT filter(-1) < 0
 ),TRUE,'Observations and estimations can be negative.');
-
-SELECT is((
-	SELECT MAX(filter(x)) > avg(x)
-	FROM generate_series(1,10) AS x
-),FALSE,'Estimation is not above the mean.');
 
 
 SELECT * FROM finish();
